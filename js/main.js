a3888c1dcb3caa5ef701f285a10836b26c663996
SIS-LAP-000\Sis. Toshiba <katyfigueroam.2@gmail.com>
2016-11-30 13:46:13 -0500

    pagina-avance

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+  $('#media').carousel({
+    pause: true,
+    interval: 1,
+  });
+});
+
---
+++ js/menu-scroll.js
@@ -0,0 +1,32 @@
+$(document).scroll(function() {    
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 100) {
+        $("#menu").addClass("active");
+    } else {
+        $("#menu").removeClass("active");
+    }
+
+});
+
+
+
+$(".navbar-toggle").click(function(){
+  $(this).toggleClass("open");
+});
+
+
+$(document).ready(function() {
+  $(".logotype").click(function() {
+    $("html, body").animate({scrollTop: $("#inicio").offset().top}, 1000);});
+  $(".sec").click(function() {
+    $("html, body").animate({scrollTop: $("#sobremi").offset().top}, 1000);});
+  $(".sec1").click(function() {
+    $("html, body").animate({scrollTop: $("#mistrabajos").offset().top}, 1000);});
+  $(".sec2").click(function() {
+    $("html, body").animate({scrollTop: $("#habilidades").offset().top}, 1000);});
+  $(".sec3").click(function() {
+    $("html, body").animate({scrollTop: $("#contacto").offset().top}, 1000);});
+  $(".farriba").click(function() {
+    $("html, body").animate({scrollTop: $("#inicio").offset().top}, 1000);});
+});
